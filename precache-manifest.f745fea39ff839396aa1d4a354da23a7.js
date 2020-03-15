19192e66dde9b86acc6cae28403e5345733b3457
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-03-15 20:36:28 -0200

    Updates

---
+++ precache-manifest.f745fea39ff839396aa1d4a354da23a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a261e15d345d36d526a841aeb95ef24c",
+    "url": "/Business/index.html"
+  },
+  {
+    "revision": "13701cf3c2ff6dea8398",
+    "url": "/Business/static/css/main.0d878acb.chunk.css"
+  },
+  {
+    "revision": "a3f8d824ffe0fa573aee",
+    "url": "/Business/static/js/2.179b4d36.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Business/static/js/2.179b4d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13701cf3c2ff6dea8398",
+    "url": "/Business/static/js/main.68c48be5.chunk.js"
+  },
+  {
+    "revision": "87574af72a38b114aca8",
+    "url": "/Business/static/js/runtime-main.ea1bb075.js"
+  },
+  {
+    "revision": "d32d238f63a8f5f1d20740cbd916945a",
+    "url": "/Business/static/media/download1.d32d238f.png"
+  }
+]);
